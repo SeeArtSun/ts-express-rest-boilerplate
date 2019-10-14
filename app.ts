f71614b8357365e05fddf995688abff058563ec5
SeeArtSun <seeart.sun@gmail.com>
2019-10-15 02:00:15 +0900

    add get users list
    * add tsconfig option(resolveJsonModule) for importing json file

--- app.ts
+++ app.ts
@@ -1,10 +1,16 @@
 import express from "express";
 
+import users from "./users.json";
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (_, res) => {
   res.sendStatus(200);
+});
+
+app.get("/users", (_, res) => {
+  res.json(users);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
