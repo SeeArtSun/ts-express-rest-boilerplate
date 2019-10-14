7bad21d8cd2e97113dec974ee43d7c72b0211dd3
SeeArtSun <seeart.sun@gmail.com>
2019-10-15 02:07:12 +0900

    add get method for retrieve specific user

--- app.ts
+++ app.ts
@@ -13,4 +13,15 @@ app.get("/users", (_, res) => {
   res.json(users);
 });
 
+app.get("/users/:id", (req, res) => {
+  const userID = req.params.id;
+  const user = users.find(user => user.id === userID);
+
+  if (!user) {
+    res.json({ message: `'${userID}' is not exist.` });
+  }
+
+  res.json(user);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
