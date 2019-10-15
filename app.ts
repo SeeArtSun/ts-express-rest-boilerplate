a2d5476d2b2e9b3a45d583d4ffb821c8e7c0ffd0
SeeArtSun <seeart.sun@gmail.com>
2019-10-15 15:39:27 +0900

    bodyParser first

--- app.ts
+++ app.ts
@@ -6,12 +6,12 @@ import users from "./api/v1/users";
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 const router = express.Router();
 app.use("/api/v1", router);
 router.use(users);
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (_, res) => {
   res.sendStatus(200);
