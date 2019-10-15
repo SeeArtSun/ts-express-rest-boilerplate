69f5680822f988a4067237a9084ca40053896f85
SeeArtSun <seeart.sun@gmail.com>
2019-10-15 19:55:39 +0900

    add mysql connection

---
+++ database/connection.ts
@@ -0,0 +1,11 @@
+import mysql from "mysql";
+
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  port: Number(process.env.MYSQL_PORT),
+  database: process.env.MYSQL_DATABASE
+});
+
+export default connection;
