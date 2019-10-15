8ecd3feb84fd1dd1a61d6889e3385ecc9c001132
SeeArtSun <seeart.sun@gmail.com>
2019-10-15 22:11:20 +0900

    remove query test

--- app.ts
+++ app.ts
@@ -3,8 +3,6 @@ import express from "express";
 
 import users from "./api/v1/users";
 import authorization from "./api/v1/authorization";
-import db from "./database/connection";
-import query from "./database/query";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,28 +18,7 @@ app.use("/api/v1", router);
 // router.use(authorization);
 router.use(users);
 
-db.connect();
-
 app.get("/", async (_, res) => {
-  const createUserTableQueryString = `
-    CREATE TABLE user (
-      id          VARCHAR(36)   NOT NULL,
-      name        VARCHAR(20),
-      phonenumber VARCHAR(15),
-      birthday    VARCHAR(8),
-      status      VARCHAR(15),
-
-      CONSTRAINT USER_PK PRIMARY KEY (id)
-    );
-  `;
-
-  try {
-    const result = await query(db, createUserTableQueryString);
-    console.log(result);
-  } catch (error) {
-    console.error(error.message);
-  }
-
   res.sendStatus(200);
 });
 
