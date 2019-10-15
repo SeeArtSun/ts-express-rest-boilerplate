321442f992023afe96bfa630879412a0a5c2cbea
SeeArtSun <seeart.sun@gmail.com>
2019-10-15 19:55:58 +0900

    create user table for test

--- app.ts
+++ app.ts
@@ -3,6 +3,7 @@ import express from "express";
 
 import users from "./api/v1/users";
 import authorization from "./api/v1/authorization";
+import db from "./database/connection";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,7 +19,30 @@ app.use("/api/v1", router);
 // router.use(authorization);
 router.use(users);
 
-app.get("/", (_, res) => {
+db.connect();
+
+app.get("/", async (_, res) => {
+  const createUserTableQueryString = `
+    CREATE TABLE user (
+      id          VARCHAR(36)   NOT NULL,
+      name        VARCHAR(20),
+      phonenumber VARCHAR(15),
+      birthday    VARCHAR(8),
+      status      VARCHAR(15),
+
+      CONSTRAINT USER_PK PRIMARY KEY (id)
+    );
+  `;
+
+  db.query(createUserTableQueryString, (err, result) => {
+    if (err) {
+      console.warn(err);
+    }
+    console.log(result);
+
+    return result;
+  });
+
   res.sendStatus(200);
 });
 
